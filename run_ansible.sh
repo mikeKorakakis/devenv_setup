ffd58a4cc3d8e509c3dc856d8e1146288439626f
Mike Korakakis <mike.korakakis@gmail.com>
2024-05-19 19:40:51 +0300

    first commit

---
+++ run_ansible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Update and upgrade system packages
+sudo apt update && sudo apt upgrade -y
+
+# Install Ansible if not already installed
+if ! command -v ansible &> /dev/null
+then
+    echo "Ansible not found, installing..."
+    sudo apt install ansible -y
+fi
+
+# Run the Ansible playbook
+ansible-playbook dev_setup.yml
